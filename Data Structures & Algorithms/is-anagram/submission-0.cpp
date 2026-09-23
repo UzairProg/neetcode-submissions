90c36e817e12643dcf08cb09d105878ce396a9e1
Uzair Mohammad <programmeruzair@gmail.com>
2026-09-23 20:34:57 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mpp;
+        unordered_map<char, int> mp;
+
+        for(auto c: s){
+            mpp[c]++;
+        }
+        for(auto c: t){
+            mp[c]++;
+        }
+
+        if(mpp == mp){
+            return true;
+        }
+        return false;
+    }
+};
